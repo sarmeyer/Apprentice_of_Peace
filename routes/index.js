6ad9a21a4cffe4f3a0e7e207438ecbc5c6cc236f
Sarah <sarahmichellemeyer@gmail.com>
2016-08-30 15:15:10 -0600

    main, about, contact pages converted to jade and in express

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('main');
+});
+router.get('/about', function(req, res, next){
+  res.render('about');
+})
+router.get('/services', function(req, res, next){
+  res.render('services');
+})
+router.get('/product', function(req, res, next){
+  res.render('product');
+})
+router.get('/blog', function(req, res, next){
+  res.render('blog');
+})
+router.get('/contact', function(req, res, next){
+  res.render('contact');
+})
+router.get('/schedule', function(req, res, next){
+  res.render('schedule');
+})
+
+module.exports = router;
