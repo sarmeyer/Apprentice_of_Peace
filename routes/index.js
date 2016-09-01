e5e742e7b872a51a99eecd62a3b7fc4a51c4a8b0
Sarah <sarahmichellemeyer@gmail.com>
2016-09-01 15:59:42 -0600

    working on path following payment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var stripe = require("stripe")("sk_test_je4ROTgvjNdXQ3tqF3iB8wrJ");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,5 +24,38 @@ router.get('/contact', function(req, res, next){
 router.get('/schedule', function(req, res, next){
   res.render('schedule');
 })
+
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Apprentice of Peace'
+  });
+});
+
+router.post('/order', function(req, res) {
+  var token = req.body.stripeToken;
+      stripe.orders.create({
+        currency: 'usd',
+        items: [{
+            type: 'sku',
+            parent: 'sku_97DNpYPYh0SCZq'
+          }],
+        shipping: {
+          name: req.body.stripeShippingName,
+          address: {
+            line1: req.body.stripeShippingAddressLine1,
+            city: req.body.stripeShippingAddressCity,
+            country: req.body.stripeShippingAddressCountry,
+            postal_code: req.body.stripeShippingAddressZip
+          }
+        },
+        email: req.body.stripeEmail
+      }).then(function(order){
+        stripe.orders.pay(order.id, {
+          source: token
+        }, function(err, order) {
+        });
+          res.render('/products', {thanks: "Thank you for your purchase!"});
+      })
+    })
 
 module.exports = router;
