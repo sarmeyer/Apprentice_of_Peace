e02f302a4451d316063794a4f754c215ecacd7b5
Sarah <sarahmichellemeyer@gmail.com>
2016-09-01 14:32:33 -0600

    styling changes and added stripe button to products page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/main', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('main');
 });
 router.get('/about', function(req, res, next){
